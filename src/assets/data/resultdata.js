6a47b85d1de0ecd993ddc73559b19269d69faf1c
gmltmd021 <gmltmd021@naver.com>
2024-12-09 17:06:25 +0900

    add result page

---
+++ src/assets/data/resultdata.js
@@ -0,0 +1,11 @@
+import RussianBlue from '../러시안블루.jpeg';
+
+export const ResultData = [
+    {
+        id: 1,
+        name: '러시안 블루',
+        best: 'ESTJ',
+        desc: '겁이 많아요',
+        image: `${RussianBlue}`  
+    },
+]
