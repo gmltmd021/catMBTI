778383efdba42ae3dc0ac9d3a2dc7aef554cbe74
gmltmd021 <gmltmd021@naver.com>
2024-12-09 16:22:06 +0900

    add question logic

--- src/assets/data/questiondata.js
+++ src/assets/data/questiondata.js
@@ -6,4 +6,32 @@ export const QuestionData = [
         answerb: "답변2",
         type: "EI",
     },
+    {
+        id:2,
+        title: "문제2",
+        answera: "답변1",
+        answerb: "답변2",
+        type: "EI",
+    },
+    {
+        id:3,
+        title: "문제3",
+        answera: "답변1",
+        answerb: "답변2",
+        type: "EI",
+    },
+    {
+        id:4,
+        title: "문제4",
+        answera: "답변1",
+        answerb: "답변2",
+        type: "EI",
+    },
+    {
+        id:5,
+        title: "문제5",
+        answera: "답변1",
+        answerb: "답변2",
+        type: "EI",
+    },
 ]
