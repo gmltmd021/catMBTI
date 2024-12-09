8e56de760c17f3b4a9197f48ab93e8ae52d783bd
gmltmd021 <gmltmd021@naver.com>
2024-12-09 14:21:55 +0900

    add page route basic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,16 @@ import React from "react";
 import styled from "styled-components";
 import PangImage from '../assets/cat1.jpeg';
 import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleClickButton = () => {
+    // useHistory 
+    navigate('/question');
+  }
+
   return (
     <Wrapper>
       <Header>예비집사 판별기</Header>
@@ -14,7 +22,7 @@ const Home = () => {
           <img src={PangImage} className="rounded-circle" width={350} height={350}></img>
         </LogoImage>
         <Desc>MBTI를 기반으로 하는 나랑 잘맞는 고양이 찾기!</Desc>
-        <Button>테스트 시작하기</Button>
+        <Button style={{ fontFamily: "심경하체" }} onClick={handleClickButton}>테스트 시작하기</Button> 
       </Contents>
     </Wrapper>
   )
@@ -32,6 +40,7 @@ const Header = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    font-family: "심경하체";
 `
 
 const Contents = styled.div`
@@ -44,6 +53,7 @@ const Contents = styled.div`
 const Title = styled.div`
     font-size: 30pt;
     margin-top: 40px;
+    font-family: "심경하체";
 `
 
 const LogoImage = styled.div`
@@ -53,4 +63,5 @@ const LogoImage = styled.div`
 const Desc = styled.div`
     font-size: 20pt;
     margin-top: 20px;
+    font-family: "심경하체";
 `
