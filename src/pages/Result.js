5e73fbb1b90108ee40176379a019e390426459dc
gmltmd021 <gmltmd021@naver.com>
2024-12-10 16:29:21 +0900

    add result logic

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -2,11 +2,22 @@ import React from "react";
 import styled from "styled-components";
 //import PangImage from '../assets/cat1.jpeg';
 import Button from "react-bootstrap/Button";
-import { useNavigate } from "react-router-dom";
 import { ResultData } from '../assets/data/resultdata';
+import { useSearchParams, useNavigate } from "react-router-dom";
 
 const Result = () => {
     const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
+    const mbti = searchParams.get('mbti');
+    // 최종 결과 데이터 객체 
+    const [resultdata, setResultData] = React.useState({});
+
+    React.useEffect(() => {
+        const result = ResultData.find((s) => s.best === mbti);
+        setResultData(result);
+    },[mbti])
+
+    console.log(mbti);
 
     return (
         <Wrapper>
@@ -14,9 +25,9 @@ const Result = () => {
             <Contents>
                 <Title>결과 보기</Title>
                 <LogoImage>
-                    <img src={ResultData[0].image} className="rounded-circle" width={350} height={350}></img>
+                    <img src={resultdata.image} className="rounded-circle" width={350} height={350}></img>
                 </LogoImage>
-                <Desc>예비 집사님과 찰떡궁합인 고양이는 {ResultData[0].name}입니다.</Desc>
+                <Desc>예비 집사님과 찰떡궁합인 고양이는 {resultdata.name}입니다.</Desc>
                 <Button style={{ fontFamily: "심경하체" }} onClick={() => navigate("/")}>
                     테스트 다시하기
                 </Button>
